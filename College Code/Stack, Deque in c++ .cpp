20a054994c569a9e16cf5ae2a2ea52cb94a649ea
ARPIT KATIYAR <94009815+arpit5220@users.noreply.github.com>
2022-05-20 10:35:04 +0530

    Create Stack, Deque in c++ .cpp

---
+++ College Code/Stack, Deque in c++ .cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+//#include <stack>
+using namespace std;
+
+// int main()
+// {
+//     stack<int> stk;     // to declare a stack 
+//     cout<<stk.empty();   // to check empty or not ,if empty then return 1 else 0;
+//      cout<<endl;
+//     stk.push(10);
+//     stk.push(20);
+//     stk.push(30);
+//     stk.push(40);
+//     int x=stk.top();  // to get top 
+//     cout<<x;
+//     cout<<endl;
+//     stk.pop();         // pop from stack
+//     int t=stk.top();
+//     cout<<t;
+    
+      
+// }
+
+// int main(){
+    
+//     Stack<int> s;
+//     int t=8;
+//     while(t!0)
+//     {
+//         int rem=t%2;
+//         s.push(rem);
+//           t=t/2;
+//     }
+    
+//       while(!s.empty())
+//       {
+//           cout<<s.top();
+//           s.pop();
+//       }
+    
+// }
+
+// #include<deque> 
+// int main()
+// {
+//     deque<int> d;    // initialise a deque (insert and delete from both end);
+    
+//     d.push_back(10);  /// push in back
+//     d.push_back(20);
+//     d.push_back(30);
+//     d.push_back(40);
+    
+//     for(int i:d)
+//     {
+//         cout<<i<<" ";
+//     }
+    
+//     cout<<endl;
+    
+//     d.push_front(60);  // push in front
+//     d.push_front(70);
+//     d.push_front(80);
+//     d.push_front(90);
+    
+//     for(int i:d)
+//     {
+//         cout<<i<<" ";
+//     }
+    
+//     cout<<endl;
+//     d.pop_front();   // pop from front
+//     for(int i:d)
+//     {
+//         cout<<i<<" ";
+//     }
+    
+//     d.pop_back();   // pop from back
+//     cout<<endl;
+    
+//     for(int i:d)
+//     {
+//         cout<<i<<" ";
+//     }
+    
+//     d.erase(d.begin(),d.begin()+2);
+    
+//     cout<<endl;
+    
+//     for(int i:d)
+//     {
+//         cout<<i<<" ";
+//     }
+    
+    
+// }
